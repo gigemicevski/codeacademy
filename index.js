2217b45f77c4292184487d273f373f9935a261cf
gigemicevski <52067606+gigemicevski@users.noreply.github.com>
2019-06-27 14:03:56 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,40 +1,10 @@
-function isTotallyDifferent(n, x) {
+function Kniga (naslov,avtor,godina)  {
 
-    var number = n + 1;
-    while (true) {
-
-        if (isNumberTotallyDifferentFromX(number, x)) {
-            return number;
-        }
-        number++;
-    }
+    this.naslov = naslov;
+    this.avtor = avtor;
+    this.godina = godina;
 }
 
-function isNumberTotallyDifferentFromX(number, x) {
+var kniga = new Kniga ('Alhemicarot','paulo coelho','1987' );
 
-    while (number > 0) {
-        var lastDigitNumber = number % 10;
-        if(doesDigitExistsInNumber(lastDigitNumber, x)){
-            return false;
-        }
-
-        number = Math.floor(number/10);
-    }
-    return true;
-}
-
-function doesDigitExistsInNumber(digit, numberX) {
-
-    while (numberX > 0) {
-        var lastDigitX = numberX % 10;
-        if (digit !== lastDigitX) {
-            numberX = Math.floor(numberX/10);
-            continue;
-        }
-        return true;
-    }
-    return false;
-}
-
-console.log(isTotallyDifferent(100, 213));
-
+console.log(kniga);
